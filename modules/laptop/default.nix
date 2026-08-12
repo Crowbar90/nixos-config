70a0237469aafbb0f584c43332b8530c137dcb54
Francesco Venturoli <f.venturoli@gmail.com>
2026-08-12 22:51:11 +0200

    Added common laptop hardware support

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -1,5 +1,8 @@
 { ... }:
 
 {
-  services.auto-cpufreq.enable = true;
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
 }
